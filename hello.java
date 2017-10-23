c5ae5ce4db59600f56386b6e8128b58eb8338209
cdiglio11 <33040909+cdiglio11@users.noreply.github.com>
2017-10-23 17:04:39 -0400

    Change

--- hello.java
+++ hello.java
@@ -1,15 +1,21 @@
 // a simple program to practice common Github commands 
 
+import java.util.Scanner
+
+
 public class hello {
 
     public static void main(String [] args) {
 
 
+	Scanner user_input = new Scanner(System.ini)
+			
 	
 	// added  line	
+	// test
 	// added another line
 
-        System.out.println("Hello Class!");
+        System.out.println("Hello Bob, my name is Chris");
 
 
 
